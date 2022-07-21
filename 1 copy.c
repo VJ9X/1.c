7d3222d50575517b5b48c7f49d8b84a26b021770
VJ9X <69524036+VJ9X@users.noreply.github.com>
2022-07-21 07:11:59 +0000

    Add files via upload

---
+++ 1 copy.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#define getname(var)
+
+// YEAAAH 1!!!!!!! AND IT'S CONSTANT!
+const int awesome = 1;
+
+// I HATE YOU SEVEN, YOU SUCK AND YOU'RE UGLY
+const int less_awesome_number = 7;
+
+
+float awesome_float = 2.0;
+char awesome_char = 'a';
+
+double uhmmmmmm_double = 12;
+
+int x = 3, y = 2, j = 1;
+
+
+//I LOVE 8!!!!!!
+int even_more_awesome = awesome + less_awesome_number;
+
+int main(){
+    printf("Hello World\n");
+    
+    //this is uhhhhhh the best comment ever!!!! also this is an integer
+    printf("YEAAAAH!!!! %d\n", awesome);
+    
+    //YUCK!!! I HATE SEVEN
+    printf("BOOOOOOO! %d\n", less_awesome_number);
+
+    printf("YEAAAH!!! YOU KNOW WHATS BETTER THATN %d", awesome);
+    printf("? %d\n", even_more_awesome);
+    //floating point number!!!!!1
+    printf("%f\n", awesome_float);
+    
+    printf("TOP 3 LETTERS\n");
+    printf("%d", x);
+    printf(". x");
+
+    printf("\n%d", y);
+    printf(". y");
+
+    printf("\n%d", j);
+    printf(". j");
+
+    //char is a single character, thank you github copilot
+    printf("%c\n", awesome_char);
+
+    printf("%lf\n", uhmmmmmm_double);
+    
+
+    printf("Endrecho approved of this file");
+    
+    printf("CREDITS:\n kid: finger \n programmer: VJ9X\n artist: VJ9X\n Approved by: Endrecho\n many thanks to: VJ9X\n and: VJ9X\n thank you to: knowtimer for the helpin\n tixlol:tixlol\n and: VJ9X\n i dont know what else to put this is hard ummmmm\n guuuhmmmmmmmmm\n");
+    printf("u7hhh BYE!\n");
+
+    
+    return 0;
+}
